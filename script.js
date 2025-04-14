5ae329f5dbdb9c859cb882c0fecc5e72b589da2a
derksKCodes <derksthetribal.ghub@gmail.com>
2025-04-14 13:30:46 +0300

    submitted week 5 assignemnt

---
+++ script.js
@@ -0,0 +1,32 @@
+function AddItem (){
+    const newItem = document.createElement("li");
+        newItem.textContent = "Javascript DOM manipulation";
+        document.getElementById("listItems").appendChild(newItem);    
+}
+
+
+function removeItem(){
+    const item = document.getElementById("listItems").lastElementChild;
+    if (item) {
+        document.getElementById("listItems").removeChild(item);
+    }
+}
+
+let isGreen = false;
+
+function changePageColor() {
+  if (!isGreen) {
+    document.body.style.backgroundColor = "green";
+    document.body.style.color = "red";
+    isGreen = true;
+  } else {
+    document.body.style.backgroundColor = "#f4f4f4"; // original background
+    document.body.style.color = "black"; // default text color
+    isGreen = false;
+  }
+}
+
+
+function changeContent(){
+    document.getElementById("heading").textContent = "Javascript DOM manipulation"; 
+}
